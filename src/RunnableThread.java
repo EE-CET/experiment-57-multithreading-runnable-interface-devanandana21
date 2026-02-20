d0f1fa265372d927e5bf7aa17f907e30a34c979b
devanandana21 <tve24el018@cet.ac.in>
2026-02-20 09:23:43 +0530

    Update RunnableThread.java

--- src/RunnableThread.java
+++ src/RunnableThread.java
@@ -1,12 +1,21 @@
-class MyRunnable implements Runnable {
-    // TODO: Override run() method
-    // Loop from 1 to 5 and print numbers separated by space
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread using Thread class");
+    }
 }
-
-public class RunnableThread {
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Thread using Runnable interface");
+    }
+}
+public class ThreadDemo {
     public static void main(String[] args) {
-        // TODO: Create an instance of MyRunnable
-        // TODO: Create a Thread object passing the runnable instance
-        // TODO: Start the thread
+        MyThread t1 = new MyThread();
+        t1.start();
+        MyRunnable obj = new MyRunnable();
+        Thread t2 = new Thread(obj);
+        t2.start();
     }
 }
